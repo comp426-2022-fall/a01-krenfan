0971f6251900a5130a29e578d2d241168719e1af
krenfan <kfan@email.unc.edu>
2022-09-03 16:36:02 -0400

    Attempt 7

--- server.js
+++ server.js
@@ -23,10 +23,13 @@ fs.readFile('./public/index.html', 'utf8', (err, data) => {
 		res.setHeader('Content-Type', 'text/html');
 		res.end(data);
 	});
-	server.listen(port, () => {
-		console.log('Server listening on port ${port}');
-	});
 });
+
+server.listen(port, () => {
+	console.log('Server listening on port ${port}');
+});
+
+
 // If there is an error, put it on the console error and return. 
 // Do not be nice about exiting.
 
